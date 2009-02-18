03da565d6ce60f189b5220914b5abb6a582d71a8
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-18 10:05:50 -0800

    Adding generated plugin

---
+++ init.rb
@@ -0,0 +1,13 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_reports do
+  name 'Redmine Reports plugin'
+  author 'Eric Davis'
+  url 'https://redmine.shaneandpeter.com'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'This is a plugin for Redmine reports'
+  version '0.1.0'
+  
+  requires_redmine :version_or_higher => '0.8.0'
+
+end
