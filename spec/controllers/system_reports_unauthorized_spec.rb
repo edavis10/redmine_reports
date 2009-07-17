96bf77948611c21a7c7ede42050adb3cf01d6c6a
Eric Davis <edavis@littlestreamsoftware.com>
2009-07-17 14:46:41 -0700

    Moved shared description specs to the helper

--- spec/controllers/system_reports_unauthorized_spec.rb
+++ spec/controllers/system_reports_unauthorized_spec.rb
@@ -1,37 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "login_required", :shared => true do
-  it 'should redirect' do
-    do_request
-    response.should be_redirect
-  end
-  
-  it 'should redirect to the login page' do
-    do_request
-    response.should redirect_to(:controller => 'account', :action => 'login', :back_url => controller.url_for(params))
-  end
-  
-end
-
-describe "denied_access", :shared => true do
-  it 'should not be successful' do
-    do_request
-    response.should_not be_success
-  end
-  
-  it 'should return a 403 status code' do
-    do_request
-    response.code.should eql("403")
-  end
-  
-  it 'should display the standard unauthorized page' do
-    do_request
-    response.should render_template('common/403')
-  end
-
-end
-
-
 describe SystemReportsController, "with a anonymous user visiting" do
   describe "#index" do
     integrate_views
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -77,3 +77,35 @@ module SystemReportsSpecHelper
 end
 
 include SystemReportsSpecHelper
+
+
+describe "login_required", :shared => true do
+  it 'should redirect' do
+    do_request
+    response.should be_redirect
+  end
+  
+  it 'should redirect to the login page' do
+    do_request
+    response.should redirect_to(:controller => 'account', :action => 'login', :back_url => controller.url_for(params))
+  end
+  
+end
+
+describe "denied_access", :shared => true do
+  it 'should not be successful' do
+    do_request
+    response.should_not be_success
+  end
+  
+  it 'should return a 403 status code' do
+    do_request
+    response.code.should eql("403")
+  end
+  
+  it 'should display the standard unauthorized page' do
+    do_request
+    response.should render_template('common/403')
+  end
+
+end
