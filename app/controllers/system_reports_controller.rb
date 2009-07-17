4a7a888282a7a9e6a38ae49dbcba08bbb4faaa7f
Eric Davis <edavis@littlestreamsoftware.com>
2009-07-17 13:46:43 -0700

    [#2755] Moved Quickbooks report to it's own controller

--- app/controllers/system_reports_controller.rb
+++ app/controllers/system_reports_controller.rb
@@ -1,17 +1,5 @@
 class SystemReportsController < ApplicationController
-  before_filter :require_admin
-
   def index
   end
 
-  def quickbooks
-    @unbilled_po = BillingExport.unbilled_po
-    @unbilled_po_total = @unbilled_po.collect {|po_item| po_item[1].to_f}.sum
-
-    @unspent_labor = BillingExport.unspent_labor
-    @unspent_labor_total = @unspent_labor.collect {|labor| labor[1].to_f}.sum
-
-    @unbilled_labor = BillingExport.unbilled_labor
-    @unbilled_labor_total = @unbilled_labor.collect {|labor| labor[1].to_f}.sum
-  end
 end
