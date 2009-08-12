1fa63b8256e2070178703fc04188f6a1904e80a6
Eric Davis <edavis@littlestreamsoftware.com>
2009-08-12 09:48:05 -0700

    [#2793] Added links to the existing global Redmine Reports.

--- app/helpers/system_reports_helper.rb
+++ app/helpers/system_reports_helper.rb
@@ -10,6 +10,14 @@ module SystemReportsHelper
                         :class => "icon #{report[:class]}"
                         )
       end
+
+      menu << link_to(l(:label_issue_view_all), { :controller => 'issues' }, :class => 'icon icon-issue')
+      menu << link_to(l(:label_overall_activity), { :controller => 'projects', :action => 'activity' }, :class => 'icon icon-activity')
+
+      menu << link_to(l(:label_details), {:controller => 'timelog', :action => 'details'}, :class => 'icon icon-time')
+      menu << link_to(l(:label_report), {:controller => 'timelog', :action => 'report'}, :class => 'icon icon-time')
+
+      
     end
   end
 end
