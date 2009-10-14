ccdbfc00f2d3f892b37277d40576a7521c3b12bd
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-14 14:22:45 -0700

    Added init file for Rails GemPlugin

---
+++ rails/init.rb
@@ -0,0 +1,23 @@
+require 'redmine'
+
+Dir[directory + '/lib/reports/**/*.rb'].each do |report|
+  require report
+end
+
+Redmine::Plugin.register :redmine_reports do
+  name 'Redmine Reports plugin'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-reports'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'This is a plugin for Redmine reports'
+  version '0.1.0'
+  
+  requires_redmine :version_or_higher => '0.8.0'
+
+  menu :top_menu, :reports, { :controller => 'system_reports', :action => 'index'}, :caption => :reports_menu, :if => Proc.new{User.current.logged?}
+
+  settings(:default => {
+             'select_size' => '5'
+           },
+           :partial => 'settings/redmine_reports_settings')
+end
